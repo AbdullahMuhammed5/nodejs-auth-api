31e636d20c8decb30c1b0e2d18650d4600b61110
abdullahmuhammed5 <abdo56668@gmail.com>
2023-06-03 16:52:03 +0200

    initialize app and connect database

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const AppError = require('./utils/appError');
+
+const userRouter = require('./routes/api/userRoutes');
+
+// Start express app
+const app = express();
+
+app.enable('trust proxy');
+
+// Implement CORS
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+    ],
+}));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '20kb' }));
+app.use(express.urlencoded({ extended: true, limit: '20kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// ROUTES
+
+module.exports = app; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE_URL.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message, err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
